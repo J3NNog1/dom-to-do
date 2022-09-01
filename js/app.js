229005e1d8dc96aaecf2d1f4c848bee3da20f508
Jenn <jenniferjuarezjj@gmail.com>
2022-09-01 15:49:34 -0400

    Dom to do final

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const input = document.getElementById('input')
+const button = document.querySelector('#submit-button')
+const uList = document.querySelector('#todo-list')
+const commentElement = document.querySelectorAll('.comment')
+
+
+button.addEventListener('click', function(evt) {
+  const li = document.createElement('li')
+  li.textContent = input.value
+  uList.appendChild(li)
+  console.log(li)
+})
+
